fb7768b2ea30c7b142db7252a71d569fc77eebb3
Andrew Newton <nootn@hotmail.com>
2012-03-26 23:20:42 +1000

    Adds NuGet generation support

--- RazorEngineFromXml/Properties/AssemblyInfo.cs
+++ RazorEngineFromXml/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RazorEngineFromXml")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This library was built to be used as an alternative to XSLT.  Using Razor in some situations might be \"cleaner\" than XSLT as you can perform Linq queries instead of XPath queries.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("http://about.me/nootn")]
 [assembly: AssemblyProduct("RazorEngineFromXml")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright http://about.me/nootn ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
